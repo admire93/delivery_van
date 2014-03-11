174931050b83e6e8a31161110e466456d92fc968
Kang Hyojun <hyojun@admire.kr>
2014-03-12 00:38:51 +0900

    Modify crawler.crawl

--- crawler/crawl.py
+++ crawler/crawl.py
@@ -12,7 +12,7 @@ def crawl(url):
     r = get(url, headers={'User-Agent': u'Mozilla/5.0'
                                         u'(Windows NT 6.3; WOW64; rv:27.0) '
                                         u'Gecko/20100101 Firefox/27.0'})
-    if r.status_code != 200 and 'text/html' not in r.headers['content-type']:
+    if r.status_code != 200 or 'text/html' not in r.headers['content-type']:
         return None
     return r.content
 
