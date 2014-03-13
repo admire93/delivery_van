39142fa92471b5f998bcab1fe0948ed0bc7152fd
Kang HyoJun <admire9@gmail.com>
2014-03-13 13:53:08 +0900

    Prepare Facebook

---
+++ gen.cfg.py
@@ -0,0 +1,12 @@
+DEBUG = True
+
+DATABASE_URL = 'sqlite:///sample.db'
+
+ALEMBIC_SCRIPT_LOCATION = '/Users/admire/src/delivery_van/dv/migrations'
+
+FACEBOOK = {
+    'app_id': '',
+    'secret': ''
+}
+
+SECRET_KEY = 'my secret key'
--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(name='delivery van',
       author='Kang Hyojun',
       author_email='hyojun@admire.kr',
       install_requires=[
-          'requests==2.2.1', 'lxml==3.3.3', 'pytest==2.5.2'
+          'requests==2.2.1', 'lxml==3.3.3', 'pytest==2.5.2',
+          'Flask-OAuthlib==0.4.3',
       ])
