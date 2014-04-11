9a0dc98f9052d4e0839a35307937acff41850bf9
Kang HyoJun <admire9@gmail.com>
2014-04-11 21:09:01 +0900

    Implement show_pager

--- dv/web/album.py
+++ dv/web/album.py
@@ -3,6 +3,7 @@ from flask import Blueprint, render_template, request
 
 from ..album import Album
 from ..db import session
+from .util import pager
 
 bp = Blueprint('album', __name__, template_folder='templates/album')
 
@@ -12,9 +13,10 @@ def all():
     page = request.args.get('page', 1, type=int)
     PAGE_PER_ARTIST = 15
     o = (page - 1) * PAGE_PER_ARTIST
-    albums = session.query(Album)\
-             .order_by(Album.created_at)\
-             .offset(o)\
-             .limit(PAGE_PER_ARTIST)\
-             .all()
-    return render_template('all.html', albums_=albums)
+    q = session.query(Album)\
+        .order_by(Album.created_at)
+    albums = q.offset(o)\
+              .limit(PAGE_PER_ARTIST)\
+              .all()
+    return render_template('all.html', albums_=albums,
+                           pages=pager(page, q.count(), PAGE_PER_ARTIST))
